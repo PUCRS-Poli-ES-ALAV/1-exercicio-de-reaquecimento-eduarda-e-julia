664a027f19f95f2278d5c320fcfc2e657cc55e82
Eduarda <eduarda.skeller@gmail.com>
2021-03-03 20:15:12 -0300

    exercicios 1

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,26 @@
 package br.pucrs.alav;
 
+import java.util.Scanner;
+
 public class Recursion {
-	
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+
+        int a, b, resultado;
+        System.out.println("Forneça dois valores");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        resultado = multiplica(a,b);
+        System.out.println("Resultado = "+ resultado);
+    }
+
+    public static int multiplica(int a, int b){
+        if(b == 0) {
+            return b;
+        }
+        else{
+            return a + multiplica(a, b - 1);
+        }
+    }
 }
