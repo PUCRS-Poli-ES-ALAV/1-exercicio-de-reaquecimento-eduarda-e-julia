2749a99b0b9da24de90d15a56fdffdd5c4066adc
JuliaMakowski <juliamakowski@hotmail.com>
2021-03-03 20:31:25 -0300

    reorg code

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,26 +1,17 @@
 package br.pucrs.alav;
 
-import java.util.Scanner;
-
 public class Recursion {
     public static void main(String [] args){
-        Scanner in = new Scanner(System.in);
-
-        int a, b, resultado;
-        System.out.println("Forneça dois valores");
-        a = in.nextInt();
-        b = in.nextInt();
-
-        resultado = multiplica(a,b);
+        int resultado = multiplicaE1(4,6);
         System.out.println("Resultado = "+ resultado);
     }
 
-    public static int multiplica(int a, int b){
+    public static int multiplicaE1(int a, int b){
         if(b == 0) {
             return b;
         }
         else{
-            return a + multiplica(a, b - 1);
+            return a + multiplicaE1(a, b - 1);
         }
     }
 }
