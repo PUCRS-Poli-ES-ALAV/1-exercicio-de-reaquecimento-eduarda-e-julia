2b7f7d1f422d44b6b6a94f038d6cb7f78e850146
JuliaMakowski <juliamakowski@hotmail.com>
2021-03-03 21:09:18 -0300

    exercicio 5

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -2,10 +2,10 @@ package br.pucrs.alav;
 
 public class Recursion {
     public static void main(String [] args){
-        int resultado = multiplicaE1(4,6);
-        System.out.println("Resultado E1 = "+ resultado);
-        System.out.println("Resultado E3 = "+ calculoE3(4));
+        System.out.println("Resultado E1, valores '4' e '6' = "+ multiplicaE1(4,6));
+        System.out.println("Resultado E3, valor '4' = "+ calculoE3(4));
         System.out.println("Resultado E4, palavra 'azul' = "+ inversaoE4("azul"));
+        System.out.println("Resultado E5, valor '5' = "+ seqE5(5));
     }
 
     public static int multiplicaE1(int a, int b){
@@ -25,5 +25,10 @@ public class Recursion {
     public static String inversaoE4(String palavra){
         if (palavra.isEmpty()) return palavra;
         return inversaoE4(palavra.substring(1)) + palavra.charAt(0);
+    }
+
+    public static double seqE5(int n){
+        if (n==2 || n==1) return n;
+        else return 2 * seqE5(n-1) +3*seqE5(n-2);
     }
 }
